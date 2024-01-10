dd60dea13784bc78407771ebcb68d8a72acb6a94
Taylor Dixon <taylor.dixon@crowdbotics.com>
2024-01-10 09:21:33 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "catsitting-dev-125708",
-  url: "https://catsitting-dev-125708.botics.co",
-  api: "https://catsitting-dev-125708.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "catsitting-dev-125708",
+  "url": "https://catsitting-dev-125708.botics.co",
+  "api": "https://catsitting-dev-125708.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
